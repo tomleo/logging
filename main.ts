bdc6223fc7dc3d19e6b1b067ff45373a1f66178c
Tommy <tom@energysage.com>
2025-01-08 13:39:17 -0500

    feat: add type annotations

--- main.ts
+++ main.ts
@@ -5,34 +5,47 @@ export const WARNING = 30;
 export const ERROR = 40;
 export const CRITICAL = 50;
 
-type Levels = typeof DEBUG | typeof INFO | typeof WARNING | typeof ERROR | typeof CRITICAL;
+type Levels =
+  | typeof DEBUG
+  | typeof INFO
+  | typeof WARNING
+  | typeof ERROR
+  | typeof CRITICAL;
 
-export function createLogger(level: Levels) {
-    return {
-        debug(...msgs: any[]) {
-            if (level <= DEBUG) {
-                console.log(...msgs);
-            }
-        },
-        info(...msg: any[]) {
-            if (level <= INFO) {
-                console.log(...msg);
-            }
-        },
-        warning(...msgs: any[]) {
-            if (level <= WARNING) {
-                console.log(...msgs);
-            }
-        },
-        error(...msgs: any[]) {
-            if (level <= ERROR) {
-                console.log(...msgs);
-            }
-        },
-        critical(...msgs: any[]) {
-            if (level <= CRITICAL) {
-                console.log(...msgs);
-            }
-        }
-    }
+type Logger = {
+  debug(...msgs: any[]): void;
+  info(...msg: any[]): void;
+  warning(...msgs: any[]): void;
+  error(...msgs: any[]): void;
+  critical(...msgs: any[]): void;
+};
+
+export function createLogger(level: Levels): Logger {
+  return {
+    debug(...msgs: any[]): void {
+      if (level <= DEBUG) {
+        console.log(...msgs);
+      }
+    },
+    info(...msg: any[]): void {
+      if (level <= INFO) {
+        console.log(...msg);
+      }
+    },
+    warning(...msgs: any[]): void {
+      if (level <= WARNING) {
+        console.log(...msgs);
+      }
+    },
+    error(...msgs: any[]): void {
+      if (level <= ERROR) {
+        console.log(...msgs);
+      }
+    },
+    critical(...msgs: any[]): void {
+      if (level <= CRITICAL) {
+        console.log(...msgs);
+      }
+    },
+  };
 }
